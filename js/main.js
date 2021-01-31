44e976637516d0a9142255d48ed40c4382df59ce
Jocelyme <jocelymelucas@gmail.com>
2021-01-31 16:56:06 -0500

    finalizado

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ let items=document.querySelector('#items');
 let portfolio=document.querySelector('#portfolio');
 let items2=document.querySelector('#items2');
 let cambio=document.querySelector('#cambio');
+let view=document.querySelector('#preview');
 
 function mostrarCalendar() {
     cambio.setAttribute("src", `./img/calendar.png`);
+    view.setAttribute("href", `https://jklucasy.github.io/calendario/`);
     if (items.classList.contains('activo')) {
         img.classList.remove('visible');
     } else {
@@ -18,6 +20,7 @@ function mostrarCalendar() {
 }
 function mostrarPortfolio() {
     cambio.setAttribute("src", `./img/portfolio.png`);
+    // view.setAttribute("href", `https://jklucasy.github.io/`);
     if (items2.classList.contains('activo')) {
             img.classList.remove('visible');
     } else {
